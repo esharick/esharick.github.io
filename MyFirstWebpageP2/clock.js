82cbdb05f2e27ffea1d87ffd588431511b738759
esharick <eddiesharick@gmail.com>
2024-02-13 12:53:31 -0500

    changes of 2/13

--- MyFirstWebpageP2/clock.js
+++ MyFirstWebpageP2/clock.js
@@ -7,10 +7,42 @@ radius *= 0.9;
 drawClock();
 
 function drawClock() {
+    const grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+    grad.addColorStop(0, "#333");
+    grad.addColorStop(0.5, "white");
+    grad.addColorStop(1, "#333");
+
+    ctx.beginPath();
     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
     ctx.fillStyle = "white";
     ctx.fill();
+
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+     
+    ctx.beginPath(); //starts a new fill/style 
+    ctx.arc(0, 0, radius * 0.05, 0, 2 * Math.PI);
+    ctx.fillStyle = "#333";
+    ctx.fill();
+
+    //draw numbers
+    ctx.font = radius * 0.15 + "px arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    for (let i = 1; i <= 12; i++) {
+        let ang = i * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(i.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
+    }
 }
+
+
 
 
 
--- MyFirstWebpageP6/clock.js
+++ MyFirstWebpageP6/clock.js
@@ -10,11 +10,36 @@ function drawFace() {
     grad.addColorStop(0, '#333');
     grad.addColorStop(0.5, "white");
     grad.addColorStop(1, '#333');
-    //use gradient as the style of a circle shape
+    //use gradient as the style the border of the arc
 
+    ctx.beginPath();
     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
     ctx.fillStyle = "white";
     ctx.fill();
-}
+
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.05, 0, 2 * Math.PI);
+    ctx.fillStyle = "#333";
+    ctx.fill();
+
+    //draw numbers
+    ctx.font = radius * 0.15 + "px arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    for (let i = 1; i < 13; i++) {
+        let ang = Math.PI / 6 * i; 
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(i.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
+    }
+} 
 
 
