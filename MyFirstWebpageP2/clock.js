7849f69308d8c921ce0fb833af6bebda5373da13
esharick <eddiesharick@gmail.com>
2024-02-14 18:24:43 -0500

    Update 2/14

--- MyFirstWebpageP2/clock.js
+++ MyFirstWebpageP2/clock.js
@@ -4,9 +4,16 @@ const ctx = canvas.getContext("2d");
 let radius = canvas.height / 2;
 ctx.translate(radius, radius);
 radius *= 0.9; 
-drawClock();
+setInterval(drawClock, 1000/60); //redraw the clock every second
 
 function drawClock() {
+    drawClockBackground();
+    drawHashes();
+    drawTime();
+    drawKnob();
+    drawNumbers();
+}
+function drawClockBackground() {
     const grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
     grad.addColorStop(0, "#333");
     grad.addColorStop(0.5, "white");
@@ -20,12 +27,17 @@ function drawClock() {
     ctx.strokeStyle = grad;
     ctx.lineWidth = radius * 0.1;
     ctx.stroke();
-     
+
+}
+
+function drawKnob() {
     ctx.beginPath(); //starts a new fill/style 
     ctx.arc(0, 0, radius * 0.05, 0, 2 * Math.PI);
-    ctx.fillStyle = "#333";
+    ctx.fillStyle = "black";
     ctx.fill();
+}
 
+function drawNumbers() {
     //draw numbers
     ctx.font = radius * 0.15 + "px arial";
     ctx.textAlign = "center";
@@ -42,7 +54,58 @@ function drawClock() {
     }
 }
 
+function drawHashes() {
+    ctx.strokeStyle = "black";
+    for (let i = 1; i <= 60; i++) {
+        let ang = i * Math.PI / 30;
+        let width = i % 5 == 0 ? radius * 0.02 : radius * 0.01;
+        ctx.lineWidth = width;
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.rotate(ang);
+        ctx.moveTo(0, -radius * 0.72);
+        ctx.lineTo(0, -radius * 0.76);
+        ctx.rotate(-ang);
+        ctx.stroke();
+    }
+}
 
+function drawTime() {
+    const now = new Date(); //current time
+    let hour = now.getHours() % 12;
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+    let millis = now.getMilliseconds();
 
+    //draw hour hand
+    let ang = (Math.PI / 6 * hour) + (Math.PI / 6 * minute / 60)
+        + (Math.PI / 6 * second / 3600);
+    let length = radius * 0.5;
+    let width = radius * 0.04;
+    drawHand(ang, length, width);
+
+    //draw minute hand
+    ang = (Math.PI / 30 * minute) + (Math.PI / 30 * second / 60);
+    length = radius * 0.75;
+    width = radius * 0.04;
+    drawHand(ang, length, width);
+
+    //draw second hand
+    ang = (Math.PI / 30 * second) + (Math.PI / 30 * millis / 1000);
+    width = radius * 0.02;
+    drawHand(ang, length, width, "red");
+}
+
+function drawHand(ang, length, width, color="black") {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0, 0);
+    ctx.rotate(ang);
+    ctx.lineTo(0, -length);
+    ctx.rotate(-ang);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+}
 
 
--- MyFirstWebpageP6/clock.js
+++ MyFirstWebpageP6/clock.js
@@ -3,7 +3,69 @@ const ctx = canvas.getContext("2d");
 let radius = canvas.height / 2;
 ctx.translate(radius, radius);
 radius *= 0.9;
-drawFace();
+setInterval(drawClock, 1000/60); //call drawClock() every 1 second
+
+function drawClock() {
+    drawFace();
+    drawNumbers();
+    drawHashes();
+    drawTime();
+    drawKnob();
+}
+
+function drawTime() {
+    const now = new Date(); //current time
+    let hour = now.getHours() % 12;
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+    let milli = now.getMilliseconds();
+
+    let ang = (Math.PI / 6 * hour) + (Math.PI / 6 * minute / 60) +
+        (Math.PI / 6 * second / 3600);
+    let length = radius * 0.5;
+    let width = radius * 0.05;
+    drawHand(ang, length, width); //hour hand
+
+    //draw the minute hand
+    ang = (Math.PI / 30 * minute) + (Math.PI / 30 * second / 60);
+    length = radius * 0.75;
+    width = radius * 0.045;
+    drawHand(ang, length, width);
+    
+    //draw the second hand
+    ang = (Math.PI / 30 * second) + (Math.PI / 30 * milli / 1000);
+    width = radius * 0.02;
+    drawHand(ang, length, width, "red");
+}
+
+function drawHashes() {
+    ctx.strokeStyle = "#333";
+    for (let i = 0; i < 60; i++) {
+        ctx.lineWidth = (i % 5 == 0) ? 0.02 * radius : 0.01 * radius;
+        let ang = Math.PI / 30 * i;
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.rotate(ang);
+        ctx.moveTo(0, -radius * 0.72);
+        ctx.lineTo(0, -radius * 0.76);
+        ctx.rotate(-ang);
+        ctx.stroke();
+    }
+
+}
+
+function drawHand(ang, length, width, color="#333") { 
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0, 0);
+    ctx.rotate(ang);
+    ctx.lineTo(0, -length);
+    ctx.rotate(-ang);
+    ctx.stroke(); //draw the path
+}
+
 function drawFace() {
     const grad = ctx.createRadialGradient(
         0, 0, radius * 0.95, 0, 0, radius * 1.05)
@@ -20,17 +82,24 @@ function drawFace() {
     ctx.strokeStyle = grad;
     ctx.lineWidth = radius * 0.1;
     ctx.stroke();
-
+}
+ 
+function drawKnob() {    
     ctx.beginPath();
     ctx.arc(0, 0, radius * 0.05, 0, 2 * Math.PI);
     ctx.fillStyle = "#333";
     ctx.fill();
+}
 
+function drawNumbers() {
     //draw numbers
     ctx.font = radius * 0.15 + "px arial";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
+    ctx.fillStyle = "#333";
+    ctx.beginPath();
     for (let i = 1; i < 13; i++) {
+        
         let ang = Math.PI / 6 * i; 
         ctx.rotate(ang);
         ctx.translate(0, -radius * 0.85);
