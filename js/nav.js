c9bc796987e90e77eb83a955ee24d699465f2695
esharick <eddiesharick@gmail.com>
2026-05-11 16:37:27 +0000

    Update to use nav.js class

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navHTML = `
+      <nav>
+        <a href="../index.html">Home</a>
+        <a href="../pages/grade9.html">Grade 9</a>
+        <a href="../pages/grade10.html">Grade 10</a>
+        <a href="../pages/grade11.html">Grade 11</a>
+        <a href="../pages/grade12.html">Grade 12</a>
+        <a href="../pages/program.html">Program of Studies</a>
+      </nav>
+    `;
+
+    document.getElementById("navbar").innerHTML = navHTML;
+});
