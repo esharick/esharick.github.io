9dc4de8c18a50245d1abb01d8f68bed0f871f078
esharick <eddiesharick@gmail.com>
2026-05-11 16:54:33 +0000

    changed to absolute paths

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navHTML = `
       <nav>
-        <a href="../index.html">Home</a>
-        <a href="../pages/grade9.html">Grade 9</a>
-        <a href="../pages/grade10.html">Grade 10</a>
-        <a href="../pages/grade11.html">Grade 11</a>
-        <a href="../pages/grade12.html">Grade 12</a>
-        <a href="../pages/program.html">Program of Studies</a>
+        <a href="/index.html">Home</a>
+        <a href="/pages/grade9.html">Grade 9</a>
+        <a href="/pages/grade10.html">Grade 10</a>
+        <a href="/pages/grade11.html">Grade 11</a>
+        <a href="/pages/grade12.html">Grade 12</a>
+        <a href="/pages/program.html">Program of Studies</a>
       </nav>
     `;
 
